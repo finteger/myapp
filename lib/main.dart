694ab8e9202a10fa9817ebe5a95237c609d93230
nashassociatesinc <nashassociatesinc@gmail.com>
2025-08-19 21:14:02 +0000

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/screens/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: Home_Page()
+    );
   }
 }

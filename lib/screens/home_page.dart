a8e479ec840015d556bff18a797cb126b352a9b9
nashassociatesinc <nashassociatesinc@gmail.com>
2025-08-25 21:14:23 +0000

    Update

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+//blueprint for task
+class Task {
+  final String id;
+  final String name;
+  final bool completed;
+
+  Task({required this.id, required this.name, required this.completed});
+
+  factory Task.fromMap(String id, Map<String, dynamic> data) {
+    return Task(
+      id: id,
+      name: data['name'] ?? '',
+      completed: data['completed'] ?? false,
+    );
+  }
+}
+
+//Define a Task Service to handle Firestore operationss
+class TaskService {
+  //Firestore instance in an alias
+  final FirebaseFirestore db = FirebaseFirestore.instance;
+
+  //Future that returns a list of tasks using factory method defined in Task class
+  Future<List<Task>> fetchTasks() async {
+    //call get to retrieve all of the documents inside the collection
+    final snapshot = await db.collection('tasks').orderBy('timestamp').get();
+    //snapshot of all documents is being mapped to factory object template
+    return snapshot.docs
+        .map((doc) => Task.fromMap(doc.id, doc.data()))
+        .toList();
+  }
+}
+
+//create a task provider to manage state
+class TaskProvider extends ChangeNotifier {}
 
 class Home_Page extends StatefulWidget {
   const Home_Page({super.key});
@@ -10,8 +48,6 @@ class Home_Page extends StatefulWidget {
 class _Home_PageState extends State<Home_Page> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Hello')
-      );
+    return const Scaffold(body: Text('Hello'));
   }
 }

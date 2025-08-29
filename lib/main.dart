b37c83783bf7b3e46404ae79284d223b31e81100
nashassociatesinc <nashassociatesinc@gmail.com>
2025-08-29 13:44:43 +0000

    UpdatE

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:myapp/screens/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
+import 'package:myapp/providers/task_provider.dart';
 
 void main() async {
   //initialize widget bindings
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => TaskProvider(),
-      child: const MaterialApp(home: Home_Page()));
+      child: const MaterialApp(home: Home_Page()),
+    );
   }
 }
---
+++ lib/models/tasks.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+//blueprint for task
+class Task {
+  final String id;
+  final String name;
+  final bool completed;
+
+  Task({required this.id, required this.name, required this.completed});
+
+  factory Task.fromMap(String id, Map<String, dynamic> data) {
+    return Task(
+      id: id,
+      name: data['name'] ?? '',
+      completed: data['completed'] ?? false,
+    );
+  }
+}
